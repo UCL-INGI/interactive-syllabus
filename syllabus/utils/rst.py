3531878a2f55bf4cc09b706a2cec6699ba20c292
François Michel <francois.michel@student.uclouvain.be>
2017-03-18 10:41:15 +0100

    add rst production helpers

---
+++ syllabus/utils/rst.py
@@ -0,0 +1,20 @@
+def hyperlink(text, target):
+    """
+    Here are example of working targets:
+    - `/mission1/page1` -> GET this path on the current hostname
+    - `http://hostname:port/path`  -> GET this url
+    :param text: the text to display to the user
+    :param target: the hyperlink target (works like an a href)
+    :return: The rst expression that shows an hyperlink going to the specified target
+    """
+    print("`%s <%s>`__" % (text, target))
+    return "`%s <%s>`__" % (text, target)
+
+
+def h(i, word):
+    line = ''.join(['*' for x in word])
+    return line
+
+
+def bullet_list(l):
+    return "- " + "\n- ".join(l)
