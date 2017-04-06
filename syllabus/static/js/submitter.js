85151556599ed74c3c7ca6840be8b7be0c61429d
François Michel <francois.michel@student.uclouvain.be>
2017-04-06 20:51:07 +0200

    add multiple inginious tasks handling and remove sphinx_test files

--- syllabus/static/js/submitter.js
+++ syllabus/static/js/submitter.js
@@ -1,19 +1,20 @@
 function submitCode(url, taskID, questionID, code, feedbackContainer, toDisable){
-    var td = $(toDisable);
+    let td = $(toDisable);
     td.attr("disabled", true);
     $.post(url, {'taskid': taskID, 'input': JSON.stringify({[questionID]: code})}, function(data) {
-        var toParse = data.result[1];
-        for(var property in data.problems){
-            toParse += "\\n" + data.problems[property];
+        let toParse = "**" + data.result[1] + "**";
+        for(let property in data.problems){
+            toParse += "\n\n" + data.problems[property];
         }
+        console.log(toParse);
         parseRST(toParse, data.result[0], feedbackContainer, td);
     });
 }
 
 function parseRST(rst, status, feedbackContainer, toEnable){
     $.post("/parserst", {rst: rst}, function(data){
-        var container = $(feedbackContainer);
-        var result = data["result"];
+        let container = $(feedbackContainer);
+        let result = data["result"];
         if(status == "failed"){
             container.removeClass("alert-success");
             container.addClass("alert-danger");
