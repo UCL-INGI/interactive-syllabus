875dda51448f0f5bcd0317177d008517d4a6fb10
François Michel <francois.michel@student.uclouvain.be>
2017-06-28 16:24:18 +0200

    create page directory when not present at launch of webapp

--- syllabus-webapp.py
+++ syllabus-webapp.py
@@ -1,4 +1,9 @@
 from syllabus.inginious_syllabus import main
+from syllabus import get_root_path
+import os
 
 if __name__ == '__main__':
+    path = os.path.join(get_root_path(), "pages")
+    if not os.path.isdir(path):
+        os.mkdir(path)
     main()
