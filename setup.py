7e039ac16dbf926ccd1160f649602f4e793a33fc
Michel François <francois.michel@student.uclouvain.be>
2018-03-07 00:57:26 +0100

    add sqlalchemy in the dependances

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     scripts= ['syllabus-webapp'],
     install_requires=[
         'pyyaml >= 3.12', 'werkzeug >= 0.11.11', 'pygments >= 2.1.3', 'flask >= 0.12', 'docutils >= 0.13.1', 'lti',
-        'flask-sqlalchemy >= 2.3.2', 'python3-saml', 'GitPython'
+        'flask-sqlalchemy >= 2.3.2', 'sqlalchemy','python3-saml', 'GitPython'
     ],
     include_package_data=True,
     description='This is an interactive syllabus, that allows to write rST pages, with INGInious exercises inside these pages '
