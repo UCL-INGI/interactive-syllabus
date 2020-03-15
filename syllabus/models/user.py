747f4978806935113392cc5b5a1f573054248d25
Alexandre Dubray <alexandre.dubray@student.uclouvain.be>
2020-03-15 13:13:51 +0100

    remove activation secret from database

--- syllabus/models/user.py
+++ syllabus/models/user.py
@@ -14,10 +14,9 @@ class User(Base):
     change_password_url = Column(String(50))
     right = Column(String(30))
     activated = Column(Boolean(), nullable=False, default=False)
-    activation_secret = Column(String(80), unique=True, index=True)
 
     def __init__(self, name, email, hash_password, full_name=None, change_password_url=None, right=None,
-                 activated=False, activation_secret=None):
+                 activated=False):
         self.username = name
         self.email = email
         self.hash_password = hash_password
@@ -25,7 +24,6 @@ class User(Base):
         self.change_password_url = change_password_url
         self.right = right
         self.activated = activated
-        self.activation_secret = activation_secret
 
     def to_dict(self):
         return {"username": self.username, "email": self.email, "right": self.right}
@@ -48,11 +46,15 @@ class User(Base):
 
 def hash_password_func(email, password, global_salt, n_iterations):
     if global_salt is None:
-        return sha512(password).hexdigest()
+        return sha512(password.encode()).hexdigest()
     return pbkdf2_hmac('sha512',
                        password=password.encode(),
                        salt=HMAC(global_salt.encode(), email.encode(), sha256).digest(),
                        iterations=n_iterations)
+
+
+def get_activation_hash(email, secret):
+    return HMAC(secret.encode(), email.encode(), sha256).hexdigest()
 
 
 class UserAlreadyExists(Exception):
